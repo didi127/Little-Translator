d9f8d203d4fd7f892674fed64a59b02cdd13bf9e
”迪迪“ <dd1279078070@gmail.com>
2023-03-18 12:36:15 +0800

    first

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Little-Translator
-Little Translator
+
+Little Translator是一个基于React + TypeScript  + vite可以实现 各种语言 翻译到 中文的项目
+
+```
+## 安装依赖
+npm install
+
+## 启动项目
+npm run dev
+```
+
+![image-20230318122738778](C:\Users\12790\AppData\Roaming\Typora\typora-user-images\image-20230318122738778.png)
+
--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,6 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import md5 from 'js-md5';
 
-// interface WordStatisticResult {
-//   // : string;
-//   // translatedText: string;
-//   wordCount: number;
-//   wordStart: string;
-//   wordStartCount: number;
-// }
-
 interface WordStatisticResult {
   wordCount: number;
   wordStart: string;
@@ -26,7 +18,6 @@ function App() {
   const [shuruText, setShuruText] = useState('input')
   const [statisticText, setStatisticText] = useState('Statistics: there are 0 words, the word of "i" appears 0 times')
   const [wordStatisticResult, setWordStatisticResult] = useState<WordStatisticResult | null>(null);
-  // const [inputValue, setInputValue] = useState(""); 
 
   const appid = '20230316001603096';
   const key = '3wLMHej2nPWxD8qj83eM';
@@ -35,20 +26,10 @@ function App() {
   // 多个query可以用\n连接  如 query='apple\norange\nbanana\npear'
   const from = 'auto';
   const to = 'zh';
-  // let wordCount2 = 0;
-  // let wordStart2 ='I';
-  // let wordStartCount2 = 0;
-  
+
   // 调用翻译API的函数
   const translateText = async () => {
     const query = `${inputText}`;
-    if (query == null || query == undefined || query == '') {
-      // 提示不可输入空值
-      // <Alert title="不可输入空值" type="error" />
-    }
-    // if (!query) {
-    //   return <Alert title="不可输入空值" type="error" />
-    // }
 
     const wordSplit = query.split(' ');
     const wordCount2 = wordSplit.length;
@@ -76,8 +57,6 @@ function App() {
     setButtonText(`翻译`);
     setTitleText(`小小翻译家`);
     setShuruText('输入')
-    // const aa = `统计数据: 有${wordStatisticResult.wordCount}个词, ${wordStatisticResult.wordStart}这个单词出现了${wordStatisticResult.wordStartCount}次。`;
-    // const aa = `统计数据: 有${wordStatisticResult.wordCount}个词, `;
     setStatisticText(`统计数据: 有${wordCount2}个词, ${wordStart2}这个单词出现了${wordStartCount2}次。`);
   } catch(error) {
     console.log(error);
@@ -90,7 +69,6 @@ function App() {
       <div className='hezi'> 
         <header className="App-header">
           <h1>{titleText}</h1>
-          {/* <h1>小小翻译家</h1> */}
         </header>
         <button className='buttonEl' onClick={translateText}>{ buttonText }</button>
       </div>
@@ -102,14 +80,8 @@ function App() {
           onChange={(e) => setInputText(e.target.value)}
           placeholder="input your words here"
         />
-        
-        
           <div>
-            {/* <h3 className='yuanwen'>原文</h3> */}
-            {/* <p>{translationResult.}</p> */}
-            {/* <h3>翻译结果</h3> */}
             <p className='fanyijieguo'>{translatedText}</p>
-            {/* <p className='tongji'>统计数据: 有{wordStatisticResult.wordCount}个词, {wordStatisticResult.wordStart}这个单词出现了  {wordStatisticResult.wordStartCount}次。</p> */}
             <p className='tongji'>{statisticText}</p>
           </div>
         
